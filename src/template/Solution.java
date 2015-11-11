7bc6e91df8d03976a1130cbec42a95af516f8f50
Lorizio <Lorizio@users.noreply.github.com>
2015-11-11 15:25:13 +0100

    Added toPlans() in Solution

--- src/template/Solution.java
+++ src/template/Solution.java
@@ -1,9 +1,14 @@
 package template;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import logist.plan.Plan;
+import logist.simulation.Vehicle;
+import logist.task.Task;
+import logist.task.TaskSet;
+import logist.topology.Topology.City;
 
 public class Solution {
 
@@ -11,34 +16,89 @@ public class Solution {
 	Integer[] nextTaskVehicles;
 	Integer[][] time;
 	Integer[] vehicles;
-	
+
 	public Solution(int Nv, int Nt) {
 		nextTaskActions = new Integer[2*Nt];
 		nextTaskVehicles = new Integer[Nv];
 		time = new Integer[Nv][2*Nt];
 		vehicles = new Integer[Nt];
 	}
-	
+
 	public Integer getNextTaskActions(int index) {
 		return nextTaskActions[index];
 	}
-	
+
 	public void setNextTaskActions(int index,int newValue) {
 		nextTaskActions[index] = newValue;
 	}
-	
+
 	public Integer getNextTaskVehicles(int index) {
 		return nextTaskVehicles[index];
 	}
-	
+
 	public void setNextTaskVehicles(int index,int newValue) {
 		nextTaskVehicles[index] = newValue;
 	}
-	
-	public List<Plan> toPlans() {
+
+	public List<Plan> toPlans(List<Vehicle> vehicles, TaskSet tasks) {
 		List<Plan> plans;
 		plans = new ArrayList<Plan>();
-		
+		List<Task> tasksList = new ArrayList<Task>();
+		for (Task t : tasks) {
+			tasksList.add(t);
+		}
+
+		// for each vehicle we will create a plan
+		for (int i = 0; i < nextTaskVehicles.length; i++) {
+
+			// if vehicle has no first task --> plan is empty and the vehicle won't be used
+			if (nextTaskVehicles[i] == null) {
+				plans.add(Plan.EMPTY);
+
+			} else {
+				City currentCity = vehicles.get(i).homeCity();
+				Plan plan = new Plan(currentCity);
+
+				// move to pick up first task
+				for (City city : currentCity.pathTo(tasksList.get(nextTaskVehicles[i]).pickupCity)) {
+					plan.appendMove(city);
+				}
+
+				currentCity = tasksList.get(nextTaskVehicles[i]).pickupCity;
+
+				plan.appendPickup(tasksList.get(nextTaskVehicles[i]));
+
+				Integer action = nextTaskActions[nextTaskVehicles[i]];
+				while (action != null) {
+					// next task id to handle = nextAction / 2
+					// pick up or deliver = nextAction % 2  /// 0: pickup, 1: deliver
+
+					int taskId = action / 2;
+					int p_Or_d = action % 2;
+
+					if (p_Or_d == 0) {
+						for (City city : currentCity.pathTo(tasksList.get(taskId).pickupCity)) {
+							plan.appendMove(city);
+						}
+						plan.appendPickup(tasksList.get(taskId));
+						currentCity = tasksList.get(taskId).pickupCity;
+						
+					} else {
+						for (City city : currentCity.pathTo(tasksList.get(taskId).deliveryCity)) {
+							plan.appendMove(city);
+						}
+						plan.appendDelivery(tasksList.get(taskId));
+						currentCity = tasksList.get(taskId).deliveryCity;
+					}
+					
+					action = nextTaskActions[action];
+				}
+				
+				plans.add(plan);
+
+			}
+		}
+
 		return plans;
 	}
 }
